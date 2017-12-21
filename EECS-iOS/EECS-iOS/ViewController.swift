9d29ab08df69e8601095564c060fde22f2b6c61b
stevietriesenberg <stevie.triesenberg@gmail.com>
2017-12-21 12:21:04 -0500

    created xcode project

---
+++ EECS-iOS/EECS-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EECS-iOS
+//
+//  Created by Stevie Triesenberg on 12/21/17.
+//  Copyright © 2017 University of Michigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
